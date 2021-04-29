bd06fc4c9bd52e4bab445e585ee3e904f6030772
edmarsht <edtoulet@gmail..com>
2021-04-29 12:38:53 +0200

    project completed

---
+++ lib/room.rb
@@ -0,0 +1,33 @@
+class Room
+    # => PART 1
+    def initialize(capacity)
+        @capacity = capacity 
+        @occupants = []
+    end
+    
+    def capacity
+        @capacity
+    end
+
+    def occupants
+        @occupants
+    end
+
+    def full? 
+        return false if @occupants.length < @capacity
+        return true if @occupants.length >= @capacity
+    end
+
+    def available_space
+        @capacity - @occupants.length 
+    end
+
+    def add_occupant(person)
+        if !self.full?
+            @occupants << person
+            return true
+        else 
+            return false
+        end
+    end
+end
